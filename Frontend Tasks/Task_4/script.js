70b32775b1943fb1020765da07ada85f508334d0
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-02-28 08:54:42 +0530

    Update script.js

--- Frontend Tasks/Task_4/script.js
+++ Frontend Tasks/Task_4/script.js
@@ -3,12 +3,9 @@ const button = document.getElementById('colorButton');
 const colors = ['#f4a261', '#2a9d8f', '#e76f51', '#264653', '#e9c46a'];
 
 function changeBackgroundColor() {
-
     const body = document.body;
-
     // to pick random color from array
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
-
     body.style.backgroundColor = randomColor;
 }
 
